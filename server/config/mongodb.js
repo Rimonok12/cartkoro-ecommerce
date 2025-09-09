f60c7612b086f390c5f96945cea3c4325b8cd62a
rimon debnath <rimonok12@gmail.com>
2025-09-09 15:04:20 +0000

    mongodb uri console

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,12 +1,12 @@
 // server/config/mongodb.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectMongo = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('✅ MongoDB connected');
+    console.log("✅ MongoDB connected::", MONGO_URI);
   } catch (err) {
-    console.error('❌ MongoDB connection error:', err.message);
+    console.error("❌ MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
