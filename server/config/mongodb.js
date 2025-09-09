a3c031c94f652ec743d831f06b49dcfd9cc2cb38
rimon debnath <rimonok12@gmail.com>
2025-09-09 15:54:07 +0000

    removed console

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 const connectMongo = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected::", process.env.MONGO_URI);
+    console.log("MongoDB connected");
   } catch (err) {
-    console.error("❌ MongoDB connection error:", err.message);
+    console.error("MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
